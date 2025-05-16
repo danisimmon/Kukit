2791901d68cd647dcd238243cee9e7a47c3466b8
Ainhoa <22360520@live.uem.es>
2025-05-16 19:13:56 +0200

    COMMIT EN EL QUE ESTA TODO BIEN VOLVER A ESTE COMMIT EN CASO DE ROMPERSE

--- frontend/kukit/src/listaCompra/listaCompra.jsx
+++ frontend/kukit/src/listaCompra/listaCompra.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ListaCompra = ({ showListaCompra, setListaCompra }) => {
+  const navigate = useNavigate();
   const offcanvasRef = useRef();
   const bsOffcanvasRef = useRef(null);
 
@@ -43,7 +45,7 @@ const ListaCompra = ({ showListaCompra, setListaCompra }) => {
         }
       );
       if (response.data.status === "success") {
-        setListaCompra(response.data.data || []);
+        setLista(response.data.data || []);
         setMensaje("Lista de compra obtenida correctamente.");
         setExito(true);
       } else {
@@ -59,36 +61,8 @@ const ListaCompra = ({ showListaCompra, setListaCompra }) => {
     }
   };
 
-  // Insertar un producto en la lista de la compra
-  const insertListaCompra = async () => {
-    try {
-      const response = await axios.post(
-        "http://localhost/api/area_privada/listaCompra/insertListaCompra.php",
-        nuevoProducto,
-        {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
-        }
-      );
-      if (response.data.success === "true") {
-        setMensaje(
-          response.data.message || "Producto añadido correctamente."
-        );
-        setExito(true);
-        getListaCompra(); // Actualizar la lista
-      } else {
-        setMensaje(response.data.message || "Error al añadir el producto.");
-        setExito(false);
-      }
-    } catch (error) {
-      setMensaje("Error al añadir el producto.");
-      setExito(false);
-      console.error(error);
-    }
-  };
-
   // Eliminar un producto de la lista de la compra
-  const deleteListaCompra = async (id_producto) => {
+  const deleteListaCompra = async (id_producto, cantidad) => {
     try {
       const response = await axios.post(
         "http://localhost/api/area_privada/listaCompra/deleteListaCompra.php",
@@ -119,7 +93,7 @@ const ListaCompra = ({ showListaCompra, setListaCompra }) => {
   const vaciarListaCompra = async () => {
     try {
       for (const producto of listaCompra) {
-        await deleteListaCompra(producto.id_producto);
+        await deleteListaCompra(producto.id_producto, producto.cantidad);
       }
       setMensaje("Lista de la compra vaciada correctamente.");
       setExito(true);
@@ -185,14 +159,14 @@ const ListaCompra = ({ showListaCompra, setListaCompra }) => {
             </ul>
           </div>
           <div className="botones-lista-compra">
-            <button
-              className="btn btn-danger"
-              onClick={vaciarListaCompra}
-            >
+            <button className="btn btn-danger" onClick={vaciarListaCompra}>
               Vaciar Lista
             </button>
-            <button className="btn btn-secondary">Ir a recetas</button>
-          </div>
+            <button
+            className="btn btn-secondary"
+              onClick={() => navigate("/recetas")} 
+            >Ir a recetas</button>
+                       </div>
         </div>
       </div>
     </>
